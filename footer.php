4acd04d93c4bff1cafe04d899f7d7d2b4a4fa94a
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-02-23 11:28:16 +0100

    basic css and structure

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <h2 href="">Adress</h2>
+    <h2 href="">Admin </h2>
+    <div>
+        <h2 href="">Medier</h2>
+        <br>    
+        <a href="">Facebook</a>
+        <br>    
+        <a href="">LinkedIn</a>
+        <br>    
+        <a href="">Instagram</a>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+?>
+<html>
+    <head>
+        <Title>Sinus Skateshop</Title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <header>
+            <img src="resource/logo/header-logo.png" alt="Sinus Logo">
+            </header>
+        <main>
+            aa
+            aa
+</main>
+        <?php include 'footer.php'; ?>
+</body>
+
