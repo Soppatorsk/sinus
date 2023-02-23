9b0a5d5cc4b56e0b4694c2ec3ac9300f50dfcf37
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-02-23 13:42:35 +0100

    index page

--- index.php
+++ index.php
@@ -1,17 +1,48 @@
-<?php 
+<?php
 
 ?>
 <html>
-    <head>
-        <Title>Sinus Skateshop</Title>
-        <link rel="stylesheet" href="css/main.css">
-    </head>
-    <body>
-        <?php include 'resource/header.php'; ?>
-        <main>
-            aa
-            aa
-</main>
-        <?php include 'resource/footer.php'; ?>
-</body>
 
+<head>
+    <Title>Sinus Skateshop</Title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <?php include 'resource/header.php'; ?>
+    <main>
+        <div class="box">
+            <a href="hoodies.php">
+                <h2>Hoodies</h2>
+                <img src="resource/products/hoodie-ash.png" alt="">
+            </a>
+        </div>
+        <div class="box">
+            <a href="tshirts.php">
+            <h2>T-shirts</h2>
+            <img src="resource/products/sinus-tshirt-blue.png" alt="">
+            </a>
+        </div>
+        <div class="box">
+            <a href="caps.php">
+            <h2>Caps</h2>
+            <img src="resource/products/sinus-cap-red.png" alt="">
+            </a>
+        </div>
+        <div class="box">
+            <a href="skateboards.php">
+            <h2>Skateboards</h2>
+            <img src="resource/products/sinus-skateboard-gretasfury.png" alt="">
+            </a>
+        </div>
+        <div class="box">
+            <a href="wheels.php">
+            <h2>Wheels</h2>
+            <img src="resource/products/sinus-wheel-wave.png" alt="">
+            </a>
+        </div>
+
+
+    </main>
+    <?php include 'resource/footer.php'; ?>
+</body>
--- resource/header.php
+++ resource/header.php
@@ -1,3 +1,4 @@
 <header>
 <img src="resource/logo/header-logo.png" alt="Sinus Logo">
+<hr>
 </header>
