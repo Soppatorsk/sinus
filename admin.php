986732ea13e03442d3544befe53de36373789c5d
SimonJsv <124794619+SimonJsv@users.noreply.github.com>
2023-03-01 15:07:36 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,5 +1,5 @@
 <?php
-// Connect to the database
+
 // Define MySQL database credentials
 $servername = "localhost";
 $username = "root";
@@ -14,7 +14,26 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo "Connected successfully";
+// Check if a form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the submitted form data
+    $product_id = $_POST["ProductID"];
+    $product_colour = $_POST["Colour"];
+    $product_size = $_POST["Size"];
+    $product_price = $_POST["Price"];
+    $product_category = $_POST["CategoryID"];
+
+    // Update the product information in the database
+    $sql = "UPDATE products SET Colour='$product_colour', Size='$product_size', Price='$product_price', CategoryID='$product_category' WHERE ProductID='$product_id'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+        echo "Product information updated successfully.";
+    } else {
+        echo "Error updating product information: " . mysqli_error($conn);
+    }
+}
+
 // Retrieve information about products
 $sql = "SELECT * FROM products";
 $result = mysqli_query($conn, $sql);
@@ -31,26 +50,6 @@ if (mysqli_num_rows($result) > 0) {
     echo "No products found.";
 }
 
-// Check if a form has been submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the submitted form data
-    $product_id = $_POST["product_id"];
-    $product_name = $_POST["product_name"];
-    $product_price = $_POST["product_price"];
-    $product_size = $_POST["product_size"];
-    $product_category = $_POST["product_category"];
-
-    // Update the product information in the database
-    $sql = "UPDATE products SET name='$product_name', price='$product_price', size='$product_size', category='$product_category' WHERE id='$product_id'";
-    $result = mysqli_query($conn, $sql);
-
-    if ($result) {
-        echo "Product information updated successfully.";
-    } else {
-        echo "Error updating product information: " . mysqli_error($conn);
-    }
-}
-
 // Close the database connection
 mysqli_close($conn);
 ?>
@@ -62,33 +61,27 @@ mysqli_close($conn);
 <body>
     <h1>Product Administration</h1>
     <table>
-        
         <tr>
             <th>ID</th>
-            <th>Name</th>
-            <th>Price</th>
+            <th>Colour</th>
             <th>Size</th>
-            <th>Category</th>
+            <th>Price</th>
+            <th>CategoryID</th>
             <th>Action</th>
         </tr>
         <?php foreach ($products as $product): ?>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <tr>
-            <td><?php echo $product["id"]; ?></td>
-            <td><input type="text" name="product_name" value="<?php echo $product["name"]; ?>"></td>
-            <td><input type="number" name="product_price" value="<?php echo $product["price"]; ?>"></td>
-            <td><input type="text" name="product_size" value="<?php echo $product["size"]; ?>"></td>
-            <td><input type="text" name="product_category" value="<?php echo $product["category"]; ?>"></td>
-            <input type="hidden" name="product_id" value="<?php echo $product["id"]; ?>">
-            <td><input type="submit" value="Update"></td>
-            </tr>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <td><?php echo $product["ProductID"]; ?></td>
+                <td><input type="text" name="Colour" value="<?php echo $product["Colour"]; ?>"></td>
+                <td><input type="text" name="Size" value="<?php echo $product["Size"]; ?>"></td>
+                <td><input type="number" name="Price" value="<?php echo $product["Price"]; ?>"></td>
+                <td><input type="text" name="CategoryID" value="<?php echo $product["CategoryID"]; ?>"></td>
+                <input type="hidden" name="ProductID" value="<?php echo $product["ProductID"]; ?>">
+                <td><input type="submit" value="Update"></td>
             </form>
+        </tr>
         <?php endforeach; ?>
-        
-        
     </table>
-    <main>
-    <?php include 'resource/footer.php'; ?>
-    </main>
 </body>
 </html>
