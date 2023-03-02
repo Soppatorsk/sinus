999bbc5b287cbc8f728aa20180f50737eb43b88e
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-03-02 14:02:40 +0100

    db fix

--- index.php
+++ index.php
@@ -10,36 +10,7 @@
 <body>
     <?php include 'resource/header.php'; ?>
     <main>
-        <div class="box">
-            <a href="shop.php">
-                <h2>Hoodies</h2>
-                <img src="resource/products/hoodie-ash.png" alt="">
-            </a>
-        </div>
-        <div class="box">
-            <a href="shop.php?product=1">
-            <h2>T-shirts</h2>
-            <img src="resource/products/sinus-tshirt-blue.png" alt="">
-            </a>
-        </div>
-        <div class="box">
-            <a href="shop.php?product=1">
-            <h2>Caps</h2>
-            <img src="resource/products/sinus-cap-red.png" alt="">
-            </a>
-        </div>
-        <div class="box">
-            <a href="shop.php?product=1">
-            <h2>Skateboards</h2>
-            <img src="resource/products/sinus-skateboard-gretasfury.png" alt="">
-            </a>
-        </div>
-        <div class="box">
-            <a href="shop.php?product=1">
-            <h2>Wheels</h2>
-            <img src="resource/products/sinus-wheel-wave.png" alt="">
-            </a>
-        </div>
+        <?php header('Location: shop.php');?>
     </main>
     <?php include 'resource/footer.php'; ?>
 </body>
