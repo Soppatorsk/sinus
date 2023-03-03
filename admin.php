6004c46038fbc56ba77943e6baae900c120bfa59
SimonJsv <124794619+SimonJsv@users.noreply.github.com>
2023-03-03 14:07:34 +0100

    Added functionality to update a description of the product.

--- admin.php
+++ admin.php
@@ -11,9 +11,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $product_size = isset($_POST["Size"]) ? $_POST["Size"] : "";
   $product_price = isset($_POST["Price"]) ? $_POST["Price"] : "";
   $product_category = isset($_POST["CategoryID"]) ? $_POST["CategoryID"] : "";
+  $product_description = isset($_POST["description"]) ? $_POST["description"] : "";
 
   // Update the product information in the database
-  $sql = "UPDATE products SET Colour='$product_colour', Size='$product_size', Price='$product_price', CategoryID='$product_category' WHERE ProductID='$product_id'";
+  $sql = "UPDATE products SET Colour='$product_colour', Size='$product_size', Price='$product_price', CategoryID='$product_category', Description='$product_description' WHERE ProductID='$product_id'";
   $result = mysqli_query($conn, $sql);
 
   if ($result) {
@@ -32,9 +33,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["new_product_id"]) && i
   $product_size = $_POST["new_size"];
   $product_price = $_POST["new_price"];
   $product_category = $_POST["new_category_id"];
+  $product_description = $_POST["new_product_description"];
+  
 
   // Insert the new product information into the database
-  $sql = "INSERT INTO products (ProductID, Colour, Size, Price, CategoryID) VALUES ('$product_id', '$product_colour', '$product_size', '$product_price', '$product_category')";
+  $sql = "INSERT INTO products (ProductID, Colour, Size, Price, CategoryID) VALUES ('$product_id', '$product_colour', '$product_size', '$product_price', '$product_category','$product_description')";
   $result = mysqli_query($conn, $sql);
 
   if ($result) {
@@ -45,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["new_product_id"]) && i
 }
 
 // Retrieve information about products
-$sql = "SELECT * FROM products";
+$sql = "SELECT ProductID, Colour, Size, Price, CategoryID, Description FROM products";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
@@ -59,6 +62,7 @@ if (mysqli_num_rows($result) > 0) {
 } else {
     echo "No products found.";
 }
+
 
 // Close the database connection
 mysqli_close($conn);
@@ -81,6 +85,7 @@ mysqli_close($conn);
             <th>Size</th>
             <th>Price</th>
             <th>CategoryID</th>
+            <th>Product Description</th>
             <th>Action</th>
         </tr>
         <?php foreach ($products as $product): ?>
@@ -91,6 +96,7 @@ mysqli_close($conn);
                 <td><input type="text" name="Size" value="<?php echo $product["Size"]; ?>"></td>
                 <td><input type="number" name="Price" value="<?php echo $product["Price"]; ?>"></td>
                 <td><input type="text" name="CategoryID" value="<?php echo $product["CategoryID"]; ?>"></td>
+                <td><input type="text" name="description" value="<?php echo $product["Description"]; ?>"></td>
                 <input type="hidden" name="ProductID" value="<?php echo $product["ProductID"]; ?>">
                 <td><input type="submit" value="Update"></td>
             </form>
@@ -120,6 +126,10 @@ mysqli_close($conn);
             <tr>
                 <td><label for="new-category-id">Category ID:</label></td>
                 <td><input type="text" id="new-category-id" name="new_category_id" required></td>
+            </tr>
+            <tr>
+                <td><label for="new-product-description">Product Description:</label></td>
+                <td><input type="text" id="new-product-description" name="new_product_description" required></td>
             </tr>
             <tr>
                 <td colspan="2"><input type="submit" value="Add product"></td>
