bcf758306293878c13dbfd80c24ee757a6502516
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-03-03 17:35:48 +0100

    new product image upload

--- admin.php
+++ admin.php
@@ -5,47 +5,58 @@ $conn = ssDbConnect();
 
 // Check if a form has been submitted for updating a product
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Get the submitted form data
-  $product_id = isset($_POST["ProductID"]) ? $_POST["ProductID"] : "";
-  $product_colour = isset($_POST["Colour"]) ? $_POST["Colour"] : "";
-  $product_size = isset($_POST["Size"]) ? $_POST["Size"] : "";
-  $product_price = isset($_POST["Price"]) ? $_POST["Price"] : "";
-  $product_category = isset($_POST["CategoryID"]) ? $_POST["CategoryID"] : "";
-  $product_description = isset($_POST["description"]) ? $_POST["description"] : "";
+    // Get the submitted form data
+    $product_id = isset($_POST["ProductID"]) ? $_POST["ProductID"] : "";
+    $product_colour = isset($_POST["Colour"]) ? $_POST["Colour"] : "";
+    $product_size = isset($_POST["Size"]) ? $_POST["Size"] : "";
+    $product_price = isset($_POST["Price"]) ? $_POST["Price"] : "";
+    $product_category = isset($_POST["CategoryID"]) ? $_POST["CategoryID"] : "";
+    $product_description = isset($_POST["description"]) ? $_POST["description"] : "";
 
-  // Update the product information in the database
-  $sql = "UPDATE products SET Colour='$product_colour', Size='$product_size', Price='$product_price', CategoryID='$product_category', Description='$product_description' WHERE ProductID='$product_id'";
-  $result = mysqli_query($conn, $sql);
+    // Update the product information in the database
+    $sql = "UPDATE products SET Colour='$product_colour', Size='$product_size', Price='$product_price', CategoryID='$product_category', Description='$product_description' WHERE ProductID='$product_id'";
+    $result = mysqli_query($conn, $sql);
 
-  if ($result) {
-      echo "Product information updated successfully.";
-  } else {
-      echo "Error updating product information: " . mysqli_error($conn);
-  }
+    if ($result) {
+        echo "Product information updated successfully.";
+    } else {
+        echo "Error updating product information: " . mysqli_error($conn);
+    }
 }
-
 
 // Check if a form has been submitted for adding a new product
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["new_colour"]) && isset($_POST["new_size"]) && isset($_POST["new_price"]) && isset($_POST["new_category_id"])) {
-  // Get the submitted form data
-  $product_colour = $_POST["new_colour"];
-  $product_size = $_POST["new_size"];
-  $product_price = $_POST["new_price"];
-  $product_category = $_POST["new_category_id"];
-  $product_description = $_POST["new_product_description"];
-  
+    // Get the submitted form data
+    $product_colour = $_POST["new_colour"];
+    $product_size = $_POST["new_size"];
+    $product_price = $_POST["new_price"];
+    $product_category = $_POST["new_category_id"];
+    $product_description = $_POST["new_product_description"];
+    $product_img = $_POST['new_product_img']; //url, not actual img
 
-  // Insert the new product information into the database
-  $sql = "INSERT INTO products (Colour, Size, Price, CategoryID, Description) VALUES ('$product_colour', '$product_size', '$product_price', '$product_category','$product_description')";
-  $result = mysqli_query($conn, $sql);
+    // Insert the new product information into the database
+    $sql = "INSERT INTO products (Colour, Size, Price, CategoryID, Description) VALUES ('$product_colour', '$product_size', '$product_price', '$product_category','$product_description')";
+    $result = mysqli_query($conn, $sql);
 
-  if ($result) {
-      echo "New product added successfully.";
-  } else {
-      echo "Error adding new product: " . mysqli_error($conn);
-  }
-  header("Refresh:0");
+    if ($result) {
+        echo "New product added successfully.";
+    } else {
+        echo "Error adding new product: " . mysqli_error($conn);
+    }
+    
+    $sql = "SELECT * FROM products order by ProductID desc"; //Get ID of new Product
+    $result = mysqli_query($conn, $sql);
+    $result = $result->fetch_assoc();
+    #var_dump($result);
+    $product_id = $result['ProductID'];
+
+    $sql = "INSERT INTO productImages VALUES ('$product_id', '$product_img')";
+    $result = mysqli_query($conn, $sql);
+
+#    header("Refresh:0");
 }
+
+
 
 // Retrieve information about products
 $sql = "SELECT ProductID, Colour, Size, Price, CategoryID, Description FROM products";
@@ -64,14 +75,18 @@ if (mysqli_num_rows($result) > 0) {
 }
 
 
+
+
 // Close the database connection
 mysqli_close($conn);
 ?>
 
 <html>
+
 <head>
- 
+
 </head>
+
 <body>
     <h1>Product Administration</h1>
     <a href="shop.php">Take me back to the shop!</a>
@@ -89,22 +104,30 @@ mysqli_close($conn);
             <th>Action</th>
         </tr>
         <?php foreach ($products as $product): ?>
-        <tr>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <td><?php echo $product["ProductID"]; ?></td>
-                <td><input type="text" name="Colour" value="<?php echo $product["Colour"]; ?>"></td>
-                <td><input type="text" name="Size" value="<?php echo $product["Size"]; ?>"></td>
-                <td><input type="number" name="Price" value="<?php echo $product["Price"]; ?>"></td>
-                <td><input type="text" name="CategoryID" value="<?php echo $product["CategoryID"]; ?>"></td>
-                <td><input type="text" name="description" value="<?php echo $product["Description"]; ?>"></td>
-                <input type="hidden" name="ProductID" value="<?php echo $product["ProductID"]; ?>">
-                <td><input type="submit" value="Update"></td>
-            </form>
-        </tr>
+            <tr>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <td>
+                        <?php echo $product["ProductID"]; ?>
+                    </td>
+                    <td><input type="text" name="Colour" value="<?php echo $product["Colour"]; ?>"></td>
+                    <td><input type="text" name="Size" value="<?php echo $product["Size"]; ?>"></td>
+                    <td><input type="number" name="Price" value="<?php echo $product["Price"]; ?>"></td>
+                    <td><input type="text" name="CategoryID" value="<?php echo $product["CategoryID"]; ?>"></td>
+                    <td><input type="text" name="description" value="<?php echo $product["Description"]; ?>"></td>
+                    <input type="hidden" name="ProductID" value="<?php echo $product["ProductID"]; ?>">
+                    <td><input type="submit" value="Update"></td>
+                </form>
+            </tr>
         <?php endforeach; ?>
     </table>
 
     <h2>Add a new product</h2>
+  Upload new image:
+  <form action="upload.php" method="post" enctype="multipart/form-data" id="ttt">
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+<br>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <table>
             <tr>
@@ -126,6 +149,10 @@ mysqli_close($conn);
             <tr>
                 <td><label for="new-product-description">Product Description:</label></td>
                 <td><input type="text" id="new-product-description" name="new_product_description" required></td>
+            </tr>
+            <tr>
+                <td><label for="new-product-description">Product Image:</label></td>
+                <td><input type="text" id="new-product-img" name="new_product_img" value="<?php if (isset($_GET['img'])) echo $_GET['img'];?>"required></td>
             </tr>
             <tr>
                 <td colspan="2"><input type="submit" value="Add product"></td>
